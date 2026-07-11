68e8b5d72048c83561f3c2b6e8b22e7df4da6c87
mutula-creator <kevmutulake@gmail.com>
2026-07-11 20:46:45 +0300

    Fix Turnstile script loading

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,10 @@ export default function RootLayout({ children }) {
         <main>{children}</main>
         <Footer />
         <WhatsAppButton />
-        <Script src="https://challenges.cloudflare.com/turnstile/v0/api.js" strategy="lazyOnload" />
+        <Script 
+          src="https://challenges.cloudflare.com/turnstile/v0/api.js"
+          strategy="beforeInteractive"
+        />
       </body>
     </html>
   );
